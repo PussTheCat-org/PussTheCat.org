b1df0f0e0fd7cb26c87d737caec4aa7f2385d00e
TheFrenchGhosty <thefrenchghosty@thefrenchghosty.me>
2026-07-05 07:56:30 +0200

    Properly fix the banner.js

--- public/banner.js
+++ public/banner.js
@@ -109,6 +109,7 @@ function injectBanner() {
     z-index: 10000;
     justify-content: space-between;
     align-items: center;
+    flex-wrap: wrap;
     box-sizing: border-box;
     font-family: Arial, Helvetica, sans-serif;
     line-height: 1.2;
@@ -144,8 +145,6 @@ function injectBanner() {
       <a href="https://pussthecat.org/donate" style="color: #ffffff; text-decoration: none; border-bottom: solid #ffffff 1px;">Click here to donate!</a>
       <a href="#" id="donation-link" style="color: #ffffff; text-decoration: none; border-bottom: solid #ffffff 1px; font-size: 11px !important; margin: 0 0 0 0.5cm;">Already donated? Click here to permanently disable the banner (requires a code)</a>
     </p>
-    <p style="margin: 27px 0 0 0; width: 100vw; text-align: center; position: relative; left: 50%; transform: translateX(-50%);">The <a href="https://pussthecat.org" style="color: #ff6a00 !important; text-decoration: none; border-bottom: solid #ff6a00 1px;" onmouseover="this.style.setProperty('color','#ff8c33','important');this.style.borderBottomColor='#ff8c33'" onmouseout="this.style.setProperty('color','#ff6a00','important');this.style.borderBottomColor='#ff6a00'">PussTheCat.org</a> website has been redesigned, check it out!</p>
-
   `;
   banner.appendChild(contentDiv);
 
@@ -171,6 +170,12 @@ function injectBanner() {
     closeBanner(7);
   };
   banner.appendChild(closeButton);
+
+  // Create redesigned message (full-width row, centered)
+  var redesignedMsg = document.createElement("p");
+  redesignedMsg.style = "flex: 0 0 100%; text-align: center; margin: 27px 0 0 0;";
+  redesignedMsg.innerHTML = 'The <a href="https://pussthecat.org" style="color: #ff6a00 !important; text-decoration: none; border-bottom: solid #ff6a00 1px;" onmouseover="this.style.setProperty(\'color\',\'#ff8c33\',\'important\');this.style.borderBottomColor=\'#ff8c33\'" onmouseout="this.style.setProperty(\'color\',\'#ff6a00\',\'important\');this.style.borderBottomColor=\'#ff6a00\'">PussTheCat.org</a> website has been redesigned, check it out!';
+  banner.appendChild(redesignedMsg);
 
   document.body.appendChild(banner); // Inject the banner into the page
 
